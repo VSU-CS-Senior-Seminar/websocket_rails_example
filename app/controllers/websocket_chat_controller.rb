28df2e333a8d922747d557f2f7f1f078eb728c8b
Zhiguang Xu <xuzhiguang@gmail.com>
2016-04-01 22:09:18 -0400

    Followed http://tgib23.github.io/blog/2014/08/03/first-post/ A couple of changes: First, in Gemfile, explictly downgrade faye-websocket to 0.10.0 Second, in config/initializers/websocket_rails.rb, use 127.0.0.1 rather than localhost on the line config.redis_options = {:host => '127.0.0.1', :port => '6379'}

---
+++ app/controllers/websocket_chat_controller.rb
@@ -0,0 +1,6 @@
+class WebsocketChatController < WebsocketRails::BaseController
+  def message_receive
+    receive_message = message()
+    broadcast_message(:websocket_chat, receive_message)
+  end
+end
